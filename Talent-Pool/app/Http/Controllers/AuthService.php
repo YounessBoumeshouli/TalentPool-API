d3f2ab7c35bfc1af417704cdfe1d1539402eb297
BoumeshouliYouness <boumeshouliyouness6@gmail.com>
2025-03-25 11:48:54 +0000

    fill the authService controller

--- Talent-Pool/app/Http/Controllers/AuthService.php
+++ Talent-Pool/app/Http/Controllers/AuthService.php
@@ -1,10 +1,30 @@
 <?php
+namespace App\Services;
 
-namespace App\Http\Controllers;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
-use Illuminate\Http\Request;
-
-class AuthService extends Controller
+class AuthService
 {
-    //
+    public function createUser(array $data)
+    {
+        return User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+            'role' => $data['role']
+        ]);
+    }
+
+    public function generateToken(User $user)
+    {
+        return JWTAuth::fromUser($user);
+    }
+
+    public function validateCredentials($email, $password)
+    {
+        $user = User::where('email', $email)->first();
+        return $user && Hash::check($password, $user->password);
+    }
 }
